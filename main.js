e14a90ce60aae85a8d26811525c61ba015401a2f
Tiến Đạt <ntd.k108cva@gmail.com>
2020-09-28 22:00:15 +0700

    update

---
+++ main.js
@@ -0,0 +1,43 @@
+let canvas = document.getElementById("game");
+let context = canvas.getContext("2d");
+
+let x = 20, y = 20;
+let dx = 5, dy = 2;
+const radius = 15;
+
+const drawBall = () => {
+    context.beginPath()
+    context.arc(x, y, radius, 0, Math.PI*2);
+    context.fillStyle = "red";
+    context.fill();
+    context.closePath();
+}
+
+// setInterval(function() {
+//     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+
+//     drawBall();
+
+//     x += 2;
+//     y += 2;
+// }, 100)
+
+const draw = () => {
+    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    drawBall();
+
+    if(x < radius || x > canvas.clientWidth - radius) {
+        dx = -dx;
+    }
+
+    if(y < radius || y > canvas.clientHeight - radius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+
+    requestAnimationFrame(draw);
+}
+
+draw();
