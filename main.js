64af030a3a630bfea22d283d4e4227f861a6791d
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-09-29 13:59:22 +0700

    update

--- main.js
+++ main.js
@@ -1,34 +1,69 @@
 let canvas = document.getElementById("game");
 let context = canvas.getContext("2d");
 
-let x = 20, y = 20;
-let dx = 5, dy = 2;
 const radius = 15;
 
-const drawBall = () => {
-    context.beginPath()
-    context.arc(x, y, radius, 0, Math.PI*2);
-    context.fillStyle = "red";
-    context.fill();
-    context.closePath();
-}
+const balls = [
+    {
+        color: "red",
+        x: Math.floor(Math.random() * (Math.floor(canvas.clientWidth - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        y: Math.floor(Math.random() * (Math.floor(canvas.clientHeight - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        dx: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+        dy: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+    },
+    {
+        color: "green",
+        x: Math.floor(Math.random() * (Math.floor(canvas.clientWidth - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        y: Math.floor(Math.random() * (Math.floor(canvas.clientHeight - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        dx: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+        dy: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+    },
+    {
+        color: "blue",
+        x: Math.floor(Math.random() * (Math.floor(canvas.clientWidth - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        y: Math.floor(Math.random() * (Math.floor(canvas.clientHeight - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        dx: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+        dy: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+    },
+    {
+        color: "yellow",
+        x: Math.floor(Math.random() * (Math.floor(canvas.clientWidth - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        y: Math.floor(Math.random() * (Math.floor(canvas.clientHeight - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        dx: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+        dy: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+    },
+    {
+        color: "white",
+        x: Math.floor(Math.random() * (Math.floor(canvas.clientWidth - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        y: Math.floor(Math.random() * (Math.floor(canvas.clientHeight - radius) - Math.ceil(radius)) + Math.ceil(radius)),
+        dx: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+        dy: Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2)) + Math.ceil(2)),
+    }
+]
 
+const drawBall = () => {
+    for (let ball of balls){
+        context.beginPath();
+        context.arc(ball.x, ball.y, radius, 0, Math.PI*2);
+        context.fillStyle = ball.color;
+        context.fill();
+        context.closePath();
+
+        ball.x += ball.dx;
+        if(ball.x < radius || ball.x > canvas.clientWidth - radius){
+            ball.dx = -ball.dx;
+        }
+
+        ball.y += ball.dy;
+        if(ball.y < radius || ball.y > canvas.clientHeight - radius){
+            ball.dy = -ball.dy;
+        }
+    }
+}   
 
 const draw = () => {
     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     drawBall();
-
-    if(x < radius || x > canvas.clientWidth - radius) {
-        dx = -dx;
-    }
-
-    if(y < radius || y > canvas.clientHeight - radius) {
-        dy = -dy;
-    }
-
-    x += dx;
-    y += dy;
-
     requestAnimationFrame(draw);
 }
 
